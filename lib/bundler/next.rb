168e18495ddab1f749c489771f86a0cebf58ed1b
chaunce <chaunce.butterfield@plansource.com>
2018-07-06 10:05:12 -0600

    v0.1.1

--- lib/bundler/next.rb
+++ lib/bundler/next.rb
@@ -1,11 +1,9 @@
 require "bundler/next/version"
 
 module Bundler
-  class Next < Bundler::Plugin::API
-    command "next"
+  module Next
+    Bundler::Plugin::API.command('next', self)
 
-    # The exec method will be called with the `command` and the `args`.
-    # This is where you should handle all logic and functionality
     def exec(command, args)
       puts "You called " + command + " with args: " + args.inspect
     end
